4699c473cf31e60479595806e86916a711f7cea8
Carlos Martin Cruz <ccruz0991@gmail.com>
2014-07-13 18:03:32 -0500

    Complete Project 1 Carlos Cruz

--- plot1.R
+++ plot1.R
@@ -1,5 +1,3 @@
-function<-plot1()
-  {
   data<-read.table("household_power_consumption.txt",header=T,sep=";",na.strings="?")
   subdata<-subset(data, data$Date == "1/2/2007" | data$Date == "2/2/2007")
   GAP1<-subdata$Global_active_power
@@ -7,4 +5,4 @@ function<-plot1()
   png(file="plot1.png",width=480,height=480)
   hist(GAP2,col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
   dev.off()
-  }
+  
